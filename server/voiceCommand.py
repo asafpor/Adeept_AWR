8eadae9607c916a9b8935c161093f8078fc90db5
asafpor <asafpor@gmail.com>
2021-12-21 17:37:50 +0000

    updates in voice reco

--- server/voiceCommand.py
+++ server/voiceCommand.py
@@ -4,19 +4,28 @@ from pydub import AudioSegment
 from pydub.playback import play
 
 from web_client import WebClient
+from WolfAssistant import BotAssistant
+import subprocess
 
 class VoiceCommand:
     def __init__(self):
         self._r= sr.Recognizer()
         self._wc = WebClient()
+        self._assist = BotAssistant()
 
-    def listen(self):
-        with sr.Microphone(device_index = 0) as source:
-            self._r.adjust_for_ambient_noise(source)
-            print("Say Something");
-            audio = self._r.listen(source)
-            print("got it");
-        return audio
+    def listen(self, timeLimit=None):
+        while True:
+            with sr.Microphone(device_index = 0) as source:
+                try:
+                    self._r.adjust_for_ambient_noise(source)
+                    print("Say Something");
+                    audio = self._r.listen(source, phrase_time_limit=timeLimit, timeout = (timeLimit+2))
+                    print("got it");                    
+                except:
+                    print ("error got exception")
+                    continue
+            return audio
+            
 
 
 
@@ -40,19 +49,36 @@ class VoiceCommand:
 
     def main(self):
         while(True):
-            audio1 = self.listen()
+            audio1 = self.listen(2)
             text = self.voice(audio1)
-            if text == 'hello' or text == "Americano" or text == "Bob":
+            if (text == 0):
+                continue
+            if text == 'hello' or text == "Americano" or "Bob" in text or "bob" in text or "bub" in text:
                 text = {}
                 #reader_voice = AudioSegment.from_wav('/home/pi/yes_please.wav')
                 #play(reader_voice)
-                call(["espeak", "-s140  -ven+18 -z" ," Yes Please"])
-                audio = self.listen()
+                output = subprocess.Popen(["espeak", "-s140  -ven+18 -z" ," Yes Please"])
+                #output.communicate()
+                #call(["espeak", "-s140  -ven+18 -z" ," Yes Please"])
+                audio = self.listen(10)
                 text = self.voice(audio, True);
+                
                 if text != 0:
                     print (text)
-                    self._wc.sendMessage(text)
-                    call(["espeak", "-s140 -ven+18 -z" , "Your command is " + text])
+                    text_lower = text.lower()
+
+                    if "what" in text_lower or "why" in text_lower or "who" in text_lower or "where" in text_lower:
+                        call(["espeak", "-s140 -ven+18 -z" , "Your command is " + text])
+                        try:
+                            answer = self._assist.answerQ(text_lower)                        
+                            call(["espeak", "-s140 -ven+18 -z" , answer])
+                        except:
+                            call(["espeak", "-s140 -ven+18 -z" , "Bob does not know"])
+                            pass
+
+                    else:
+                        self._wc.sendMessage(text)
+                        call(["espeak", "-s140 -ven+18 -z" , "Your command is " + text])
                 else:
                     call(["espeak", "-s140 -ven+18 -z" , " Please repeat"])
             
